48980805c5095640da37b70ab0aa26cf60c442a0
j <git@jorge.red>
2022-04-19 12:14:07 +0200

    style: bash best practices

--- mount_nfs.sh
+++ mount_nfs.sh
@@ -10,7 +10,7 @@ DEV="$(ip route get ${REMOTE_NFS_IP} |grep dev |awk -F'dev ' '{print $2}' |awk '
 # - REMOTE_NFS_IP
 # - REMOTE_NFS_MAC
 # - LOCAL_MOUNTPOINT
-. ${DIR}/config
+. "${DIR}/config"
 
 function clear_line {
 	echo -en "\r                                                                    "
@@ -18,7 +18,7 @@ function clear_line {
 
 function wol {
 	echo -en "Sending magic packet...\t\t"
-	sudo etherwake -D -b -i ${DEV} ${REMOTE_NFS_MAC} > /dev/null 2>&1 && echo -e $OK || echo -e $NOK
+	sudo etherwake -D -b -i "${DEV}" "${REMOTE_NFS_MAC}" > /dev/null 2>&1 && echo -e "${OK}" || echo -e "${NOK}"
 }
 
 function is_server_alive {
@@ -26,21 +26,21 @@ function is_server_alive {
 }
 
 function wait_server_alive {
-	echo -en "Waiting for $REMOTE_NFS_IP...\t"
-	while ! is_server_alive $REMOTE_NFS_IP
+	echo -en "Waiting for ${REMOTE_NFS_IP}...\t"
+	while ! is_server_alive ${REMOTE_NFS_IP}
 	do
-		echo -en $NOK
+		echo -en "${NOK}"
 		sleep 1
 		clear_line
 		echo -en "\rWaiting for ${REMOTE_NFS_IP}...\t"
 	done
-	echo -e $OK
+	echo -e "${OK}"
 }
 
 function mount_remote_nfs {
-	mkdir -p $LOCAL_MOUNTPOINT > /dev/null 2>&1 # ensure local mountpoint exists
-	echo -en "Mounting $LOCAL_MOUNTPOINT...\t\t"
-	mount $LOCAL_MOUNTPOINT && echo -e $OK || echo -e $NOK
+	mkdir -p "${LOCAL_MOUNTPOINT}" > /dev/null 2>&1 # ensure local mountpoint exists
+	echo -en "Mounting ${LOCAL_MOUNTPOINT}...\t\t"
+	mount ${LOCAL_MOUNTPOINT} && echo -e "${OK}" || echo -e "${NOK}"
 }
 
 wol
